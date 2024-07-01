30207aba6e60ca11aac7f868ba2add903ebcb288
Shobhnik13 <shobhnikw@gmail.com>
2024-07-01 14:18:48 +0530

    resolved db issue

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
-config({ path: ".env" });
+const connectionString =
+  process.env.DATABASE_URL ||
+  "";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+  const client = postgres(connectionString);
+export const db = drizzle(client, { schema });
